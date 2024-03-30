e45ff4e31fbe1df5518dbca7466e3b453cdb37c8
tggbco <vito.bica@gmail.com>
2024-03-29 17:21:53 -0700

    Added improved debugging

--- Lab-08-functions/firmware/src/asmMult.s
+++ Lab-08-functions/firmware/src/asmMult.s
@@ -1,5 +1,7 @@
 /*** asmMult.s   ***/
-/***   Intended for use with the Functions lab */
+/* SOLUTION; used to test C test harness
+ * VB 10/14/2023
+ */
     
 /* Tell the assembler to allow both 16b and 32b extended Thumb instructions */
 .syntax unified
@@ -11,7 +13,7 @@
 .align
  
 /* define and initialize global variables that C can access */
- 
+
 /* create a string */
 .global nameStr
 .type nameStr,%gnu_unique_object
@@ -24,7 +26,7 @@ nameStr: .asciz "Inigo Montoya"
 .type nameStrPtr,%gnu_unique_object
 nameStrPtr: .word nameStr   /* Assign the mem loc of nameStr to nameStrPtr */
 
-.global a_Multiplicand,b_Multiplier,rng_Error,a_Sign,b_Sign,prod_Is_Neg,a_Abs,b_Abs,init_Product,final_Product
+.global a_Multiplicand,b_Multiplier,a_Sign,b_Sign,a_Abs,b_Abs,init_Product,final_Product
 .type a_Multiplicand,%gnu_unique_object
 .type b_Multiplier,%gnu_unique_object
 .type rng_Error,%gnu_unique_object
@@ -55,7 +57,6 @@ final_Product:   .word     0
 .text
 .align
 
- /* Make the following functions globally visible */
 .global asmUnpack, asmAbs, asmMult, asmFixSign, asmMain
 .type asmUnpack,%function
 .type asmAbs,%function
@@ -92,17 +93,13 @@ asmUnpack:
 /* function: asmAbs
  *    inputs:   r0: contains signed value
  *              r1: address where to store absolute value
- *              r2: address where to store sign bit:
- *                  0 = "+", 1 = "-"
- *    outputs:  r0: Absolute value of r0 input. Same value
- *                  as stored to location given in r1
- *              memory: 
- *                  1) store absolute value in location
- *                     given by r1
- *                  2) store sign bit in location 
- *                     given by r2
- */
+ *              r2: address where to store sign bit 0 = "+", 1 = "-")
+ *    outputs:  r0: Absolute value of r0 input. Same value as stored to location given in r1
+ *              memory: store absolute value in location given by r1
+ *                      store sign bit in location given by r2
+ */    
 asmAbs:  
+
     /*** STUDENTS: Place your asmAbs code BELOW this line!!! **************/
     
 
@@ -116,20 +113,21 @@ asmAbs:
  *    inputs:   r0: contains abs value of multiplicand (a)
  *              r1: contains abs value of multiplier (b)
  *    outputs:  r0: initial product: r0 * r1
- */    
+ */ 
 asmMult:   
 
     /*** STUDENTS: Place your asmMult code BELOW this line!!! **************/
 
 
     /*** STUDENTS: Place your asmMult code ABOVE this line!!! **************/
+
    
     /***************  END ---- asmMult  ************/
 
 
     
 /* function: asmFixSign
- *    inputs:   r0: initial product: 
+ *    inputs:   r0: initial product from previous step: 
  *              (abs value of A) * (abs value of B)
  *              r1: sign bit of originally unpacked value
  *                  of A
@@ -162,58 +160,55 @@ asmFixSign:
  *           To implement asmMain, follow the steps outlined
  *           in the comments in the body of the function
  *           definition below.
- */
+ */  
 asmMain:   
     
     /*** STUDENTS: Place your asmMain code BELOW this line!!! **************/
     
     /* Step 1:
-     * call asmUnpack. Have it store the output values in 
-     * a_Multiplicand and b_Multiplier.
+     * call asmUnpack. Have it store the output values in a_Multiplicand
+     * and b_Multiplier.
      */
 
 
-    /* Step 2a:
-     * call asmAbs for the multiplicand (A). Have it store the
-     * absolute value in a_Abs, and the sign in a_Sign.
-     */
+     /* Step 2a:
+      * call asmAbs for the multiplicand (a). Have it store the absolute value
+      * in a_Abs, and the sign in a_Sign.
+      */
 
 
 
-    /* Step 2b:
-     * call asmAbs for the multiplier (B). Have it store the
-     * absolute value in b_Abs, and the sign in b_Sign.
-     */
+     /* Step 2b:
+      * call asmAbs for the multiplier (b). Have it store the absolute value
+      * in b_Abs, and the sign in b_Sign.
+      */
+
 
 
     /* Step 3:
      * call asmMult. Pass a_Abs as the multiplicand, 
      * and b_Abs as the multiplier.
      * asmMult returns the initial (positive) product in r0.
-     * Store the value returned in r0 to mem location 
-     * init_Product.
+     * In this function (asmMain), store the output value  
+     * returned asmMult in r0 to mem location init_Product.
      */
-
 
 
     /* Step 4:
      * call asmFixSign. Pass in the initial product, and the
      * sign bits for the original a and b inputs. 
      * asmFixSign returns the final product with the correct
-     * sign. 
-     * Store the value returned in r0 to mem location 
+     * sign. Store the value returned in r0 to mem location 
      * final_Product.
      */
 
 
-
-    /* Step 5:
-     * END! Return to caller. Make sure of the following:
-     * 1) Stack has been correctly managed.
-     * 2) the final answer is stored in r0, so that the C call
-     *    can access it.
-     */
-
+     /* Step 5:
+      * END! Return to caller. Make sure of the following:
+      * 1) Stack has been correctly managed.
+      * 2) the final answer is stored in r0, so that the C call 
+      *    can access it.
+      */
 
 
     
